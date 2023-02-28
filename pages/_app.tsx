dcc49dc77d65acc05e836c61f85bc277ac308be0
Fersy1998 <fersy1998.m@gmail.com>
2023-02-28 11:56:08 -0600

    add files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextPage } from 'next';
+import { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+type NextPageLayout= NextPage & {
+    getLayout?: (page: ReactElement)=>JSX.Element
+};
+type AppPropsWithLayout=AppProps & {
+  Component: NextPageLayout
 }
+function MyApp({ Component, pageProps }:AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page: any) => page );
+
+  // return (
+  //   <>
+  //     <Component {...pageProps} />
+  //   </>
+  // )
+  return getLayout( <Component {...pageProps} /> )
+}
+
+export default MyApp
